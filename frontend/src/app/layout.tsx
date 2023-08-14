0b0b667fdffd1a6e24ff86d78bc2e9b216281e47
Whillian-Sousa <whillscf@gmail.com>
2023-08-14 03:02:46 -0300

    componentes categories e Footer

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Sidebar } from './components/Sidebar'
 import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <Navbar />
       <Sidebar />
         <div className="mb-8 mt-16 mr-14">{children}</div>
+      <Footer />
       </body>
     </html>
   )
---
+++ frontend/src/app/utils/categories.ts
@@ -0,0 +1,39 @@
+const categories = [
+  {
+    name: 'Acadêmico',
+    icon: '/academico.png',
+    route: 'academic'
+  },
+  {
+    name: 'Animais',
+    icon: '/animais.png',
+    route: 'animals'
+  },
+  {
+    name: 'Cultutral',
+    icon: '/cultural.png',
+    route: 'cultural'
+  },
+  {
+    name: 'Feiras',
+    icon: '/feiras.png',
+    route: 'fairs'
+  },
+  {
+    name: 'Festival',
+    icon: '/festival.png',
+    route: 'festival'
+  },
+  {
+    name: 'Gastronômico',
+    icon: '/gastronomico.png',
+    route: 'gastronomic'
+  },
+  {
+    name: 'Musical',
+    icon: '/musical.png',
+    route: 'musical'
+  },
+
+]
+export { categories }
