14aaed291a2df881e74020da3882f84363380065
Whillian-Sousa <whillscf@gmail.com>
2023-08-15 00:31:54 -0300

    create event and components

--- frontend/src/app/(pages)/create-event/page.tsx
+++ frontend/src/app/(pages)/create-event/page.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@/app/components/Form/Input";
+import { InputFile } from "@/app/components/Form/InputFile";
 import { categories } from "@/app/utils/categories";
 
 export default function CreateEvent() {
@@ -22,6 +23,11 @@ export default function CreateEvent() {
             placeholder="Insira o endereço do seu evento"
             type="text"
           />
+          <Input
+            title="Cupom"
+            placeholder="Insira o cupom do seu evento"
+            type="text"
+          />
           <div className="grid grid-cols-2 gap-3">
             <Input
               title="Data"
@@ -34,25 +40,89 @@ export default function CreateEvent() {
               type="number"
             />
           </div>
-        </div>
-        <p className="text-blue text-base font-medium mo-4">
-          Crie seu proprio evento de maneira que...
-        </p>
-        <div className="grid grid-cols-5 gap-2">
-          {categories.map((category) => (
-            <>
-              <div className="text-blue">
-                <input type="checkbox" className="mr-2" />
-                <label htmlFor="">{category.name}</label>
-              </div>
-            </>
-          ))}
+          <p className="text-blue text-base font-medium mo-4">
+            Crie seu proprio evento de maneira que...
+          </p>
+          <div className="grid grid-cols-5 gap-2">
+            {categories.map((category) => (
+              <>
+                <div className="text-blue">
+                  <input type="checkbox" className="mr-2" />
+                  <label htmlFor="">{category.name}</label>
+                </div>
+              </>
+            ))}
+          </div>
+          <div className="mt4">
+            <p className="text-base font-medium text-blue">Valor</p>
+            <p className="text-neutral-500 text-sm font-light">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt nostrum, in quis inventore laboriosam quam? Soluta nihil, asperiores culpa expedita repudiandae laudantium molestias error ad rerum architecto alias quidem aliquam!
+            </p>
+          </div>
+          <div className="grid grid-cols-5 gap-2">
+          <Input
+                title="Preço"
+                placeholder="R$00,00"
+                type="text"
+                className="col-span-2"
+          />
+          <Input
+                title="Setor"
+                placeholder="Insira o nome do setor"
+                type="text"
+                className="col-span-3"
+          />    
+          </div>
+          <Input
+                title="Descrição"
+                placeholder="Insira o nome do setor"
+                type="textarea"
+                className="col-span-3"
+          />
         </div>
         <div className="mb-4 ml-4">
           <p className="text-blue text-2xl font-medium">Adicionar Eventos</p>
           <p className="text-blue text-base font-light">
             Crie o seu próprio evento da maneira que você preferir!
           </p>
+          <div className="mb-4">
+            <p className="text-blue text-base font-medium">Banner</p>
+            <p className="text-neutral-500 text-sm font-light">
+              Insira um banner no fomato de 336x280
+            </p>
+            <div className="w-full h-28 bg-zinc-300 rounded-3xl shadow">
+              <InputFile />  
+            </div>            
+          </div>
+
+          <div className="mb-4">
+            <p className="text-blue text-base font-medium">Flyers</p>
+            <p className="text-neutral-500 text-sm font-light">
+              Insira até três flyers
+            </p>
+            <div className="grid grid-cols-3 gap-3">
+              <div className="w-full h-28 bg-zinc-300 rounded-3xl shadow">
+                <InputFile />  
+              </div>
+              <div className="w-full h-28 bg-zinc-300 rounded-3xl shadow">
+                <InputFile />  
+              </div>
+              <div className="w-full h-28 bg-zinc-300 rounded-3xl shadow">
+                <InputFile />  
+              </div>
+            </div>
+          </div>
+          
+          <div className="mb-4">
+            <p className="text-blue text-base font-medium">Mapa do Evento</p>
+            <p className="text-neutral-500 text-sm font-light">
+              Insira o Mapa do evento indicando os setores
+            </p>
+            <div className="w-full h-56 bg-zinc-300 rounded-3xl shadow">
+              <InputFile />  
+            </div>            
+          </div>
+
         </div>
       </div>
     </div>
--- frontend/src/app/components/Form/Input.tsx
+++ frontend/src/app/components/Form/Input.tsx
@@ -4,13 +4,28 @@ interface IInput {
   placeholder: string
   type: string
   title: string
+  className?: string
 }
-export const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInput> = (
-  { placeholder, type, title, ...rest },
+export const InputBase: ForwardRefRenderFunction<HTMLInputElement & HTMLTextAreaElement, IInput> = (
+  { placeholder, type, title, className, ...rest },
   ref
 ) => {
+  if(type === 'textarea'){
+    return (
+      <div className={`mb-4 text-blue font-medium ${className}`}>
+      <label htmlFor="">{title}</label>
+      <textarea
+        className="w-full px-6 py-[5px] bg-white rounded-lg border border-teal-400"
+        placeholder={placeholder}
+        ref={ref}
+        rows={5}
+        {...rest}
+      />
+    </div>
+    )
+  }
   return (
-    <div className="mb-4 text-blue font-medium">
+    <div className={`mb-4 text-blue font-medium ${className}`}>
       <label htmlFor="">{title}</label>
       <input
         className="w-full px-6 py-[5px] bg-white rounded-lg border border-teal-400"
